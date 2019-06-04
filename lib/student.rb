85c37a8d794e8597615aec1b319daa2223a2d5b9
Kari O'Neal <triadfirewood@gmail.com>
2019-06-04 18:23:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -59,7 +59,7 @@ class Student
     sql = "SELECT * FROM students 
     WHERE name = ?
     LIMIT 1"
-    result = DB[:conn].execute(sql, name).map do |row|
+    result = DB[:conn].execute(sql, name)[0]
       Student.new(result[0], result[1], result[2])
     end
   end
