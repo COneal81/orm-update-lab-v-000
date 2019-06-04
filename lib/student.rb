9b2946837ad16e594696dccc07e339ac400449fc
Kari O'Neal <triadfirewood@gmail.com>
2019-06-04 18:03:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,13 +58,13 @@ class Student
     
   def self.find_by_name(name)
     sql = <<-SQL
-    "SELECT * FROM students 
+    SELECT * FROM students 
     WHERE name = ?
-    LIMIT 1"
+    LIMIT 1
     SQL
     DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
-    end.first
+    end
   end
   
   def update
