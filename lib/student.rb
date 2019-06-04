656467560d699e0183963fd6930217ff5932c70a
Kari O'Neal <triadfirewood@gmail.com>
2019-06-04 18:16:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -48,10 +48,10 @@ class Student
   end
     
   def self.new_from_db(row) #convert the database into a Ruby object
-    new_student.id = row[0]
-    new_student.name = row[1]
-    new_student.grade = row[2]
-    new_student = self.new
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    self.new
   end
     
   def self.find_by_name(name)
